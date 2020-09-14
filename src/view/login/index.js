00971808ddfecf2f4412052d3fcf6987bb2a7282
Ednardo Gomes <ednardo@gomes.com>
2020-09-14 16:52:11 -0300

    Usando redux para armazer no estado do ususário no store.

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import './login.css'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
+
 import firebase from '../../config/firebase'
 import 'firebase/auth'
+
+import { useSelector, useDispatch } from 'react-redux'
 
 function Login() {
 
@@ -10,10 +13,15 @@ function Login() {
     const [senha, setSenha] = useState()
     const [msgTipo, setMsgTipo] = useState()
 
+    const dispatch = useDispatch()
+
     function logar() {
         firebase.auth().signInWithEmailAndPassword(email, senha)
             .then(resultado => {
                 setMsgTipo('sucesso')
+                setTimeout(() => {
+                    dispatch({type: 'LOG_IN', usuarioEmail: email})
+                }, 2000)
             }).catch(erro => {
                 setMsgTipo('erro')
             })
@@ -21,6 +29,9 @@ function Login() {
 
     return(
         <div className="login-content d-flex align-items-center">
+
+            {useSelector(state => state.usuarioLogado) > 0 ? <Redirect to='/'/> : null}
+
             <form className="form-signin mx-auto">
                 <div className="text-center mb-4">
                     <h1 className="h3 mb-3 text-white font-weight-bold">Login</h1>
