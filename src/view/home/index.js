78fa8d7cbe1a42f2d8fd1227d242c29b180e7f52
Ednardo Gomes <ednardo@gomes.com>
2020-09-18 17:13:22 -0300

    Criando o card dos eventos.

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import './home.css'
 import {Link} from 'react-router-dom'
-import NavBar from '../../components/navbar/'
 import { useSelector } from 'react-redux'
+import NavBar from '../../components/navbar/'
+import EventoCard from '../../components/evento-card/'
 
 function Home() {
     return(
@@ -10,6 +11,9 @@ function Home() {
             <NavBar/>
             <h1>{useSelector(state => state.usuarioEmail)}</h1>
             <h1>Logado: {useSelector(state => state.usuarioLogado)}</h1>
+            <div className="row">
+                <EventoCard/>
+            </div>
         </>
     )
 }
