2d52fe58a2401b48dcc374a264d12349d9f3bd9b
Ednardo Gomes <ednardo@gomes.com>
2020-09-21 17:03:50 -0300

    Configurando os cartões do eventos para buscar dados no bando de forma dinâmica.

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,18 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './home.css'
 import {Link} from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import NavBar from '../../components/navbar/'
+import firebase from '../../config/firebase'
 import EventoCard from '../../components/evento-card/'
 
 function Home() {
+
+    const [eventos, setEventos] = useState([])
+    let listaEventos = []
+
+    useEffect(() => {
+        firebase.firestore().collection("eventos").get().then( async (resultado) => {
+            await resultado.docs.forEach(doc => {
+                listaEventos.push({
+                    id: doc.id,
+                    ...doc.data()
+                })
+            })
+
+            setEventos(listaEventos)
+        })
+    })
+
     return(
         <>
             <NavBar/>
-            <h1>{useSelector(state => state.usuarioEmail)}</h1>
-            <h1>Logado: {useSelector(state => state.usuarioLogado)}</h1>
-            <div className="row">
-                <EventoCard/>
+
+            <div className="row p-3">
+                { eventos.map(item => <EventoCard key={item.id} id={item.id} img={item.foto} titulo={item.titulo} detalhes={item.detalhes} visualizacoes={item.visualizacoes} />) }
             </div>
         </>
     )
